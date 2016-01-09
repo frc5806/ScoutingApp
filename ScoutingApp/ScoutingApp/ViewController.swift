8b510edaac45d5293ca0199a3c5c4e3891f48cbe
Luca Koval <lucakoval@me.com>
2016-01-09 12:40:22 -0500

    Here is the app
    yee

---
+++ ScoutingApp/ScoutingApp/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  ScoutingApp
+//
+//  Created by Luca on 1/9/16.
+//  Copyright (c) 2016 HMFRC. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
