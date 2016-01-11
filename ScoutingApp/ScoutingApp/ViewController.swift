94532d9617141bd01df2baef945e99bf9cc716a8
Luca Koval <lucakoval@me.com>
2016-01-11 16:18:09 -0500

    deleted shit
    yee

--- ScoutingApp/ScoutingApp/ViewController.swift
+++ ScoutingApp/ScoutingApp/ViewController.swift
@@ -9,37 +9,13 @@
 import UIKit
 import MultipeerConnectivity
 
-class ViewController: UIViewController, MCBrowserViewControllerDelegate,
-MCSessionDelegate {
-
-    let serviceType = "LCOC-Chat"
-    
-    var browser : MCBrowserViewController!
-    var assistant : MCAdvertiserAssistant!
-    var session : MCSession!
-    var peerID: MCPeerID!
+class ViewController: UIViewController{
     
     // MARK: Properties
     @IBOutlet weak var autonomousField: UITextField!
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        
-        self.peerID = MCPeerID(displayName: UIDevice.currentDevice().name)
-        self.session = MCSession(peer: peerID)
-        self.session.delegate = self
-        
-        // create the browser viewcontroller with a unique service name
-        self.browser = MCBrowserViewController(serviceType:serviceType,
-            session:self.session)
-        
-        self.browser.delegate = self;
-        
-        self.assistant = MCAdvertiserAssistant(serviceType:serviceType,
-            discoveryInfo:nil, session:self.session)
-        
-        // tell the assistant to start advertising our fabulous chat
-        self.assistant.start()
     }
 
     override func didReceiveMemoryWarning() {
@@ -49,41 +25,6 @@ MCSessionDelegate {
 
     // MARK: Actions
     @IBAction func setAutonomous(sender: UIButton) {
-        let msg = self.autonomousField.text.dataUsingEncoding(NSUTF8StringEncoding,
-            allowLossyConversion: false)
-        
-        var error : NSError?
-        
-        self.session.sendData(msg, toPeers: self.session.connectedPeers,
-            withMode: MCSessionSendDataMode.Unreliable, error: &error)
-        
-        if error != nil {
-            print("Error sending data: \(error?.localizedDescription)")
-        }
-        
-        self.updateChat(self.autonomousField.text, fromPeer: self.peerID)
-        
-        self.autonomousField.text = ""
-    }
-    
-    func updateChat(text : String, fromPeer peerID: MCPeerID) {
-        // Appends some text to the chat view
-        
-        // If this peer ID is the local device's peer ID, then show the name
-        // as "Me"
-        var name : String
-        
-        switch peerID {
-        case self.peerID:
-            name = "Me"
-        default:
-            name = peerID.displayName
-        }
-        
-        // Add the name to the message and display it
-        let message = "\(name): \(text)\n"
-        self.chatView.text = self.chatView.text + message
         
     }
-    
 }
