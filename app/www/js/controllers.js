15be29c60c17481185c83421ebf9b1d59ab7626c
FlyingGraysons <hmwildermuth@gmail.com>
2016-03-03 13:07:29 -0500

    Add sync function skeleton

--- app/www/js/controllers.js
+++ app/www/js/controllers.js
@@ -1,11 +1,22 @@
-angular.module('starter.controllers', [])
+angular.module('starter.controllers', ['sync'])
 
-.controller('SyncCtrl', function($scope, $ionicHistory) {
-	
+.controller('SyncCtrl', function($scope, $ionicHistory, syncer) {
+	$scope.$on('$ionicView.enter', function(e) {
+		syncer.sync()
+		$ionicHistory.goBack(-1);
+	});
 })
 
 .controller('SubmitCtrl', function($scope) {})
 
-.controller('DataCtrl', function($scope, Chats) {
-
+.controller('DataCtrl', function($scope, Chats, syncer) {
+	// With the new view caching in Ionic, Controllers are only called
+	// when they are recreated or on app start, instead of every page change.
+	// To listen for when this page is active (for example, to refresh data),
+	// listen for the $ionicView.enter event:
+	//
+	$scope.$on('$ionicView.enter', function(e) {
+		syncer.sync()
+		// fill in and stuff
+	});
 });
--- app/www/js/sync.js
+++ app/www/js/sync.js
@@ -1,7 +1,20 @@
 // Syncing function: populate local storage with database information
+angular.module('sync', ['ionic', 'ionic.utils'])
 
-function sync() {
+.service('syncer', ['$localstorage', function($localstorage) {
 	// do stuff
-	console.log("Syncing");
-	return 0;
-}
+	this.sync = function () {
+		// This is the function
+		$localstorage.set('name', 'Max');
+		console.log($localstorage.get('name'));
+		$localstorage.setObject('post', {
+			name: 'Thoughts',
+			text: 'Today was a good day'
+		});
+
+		var post = $localstorage.getObject('post');
+		console.log(post);
+		console.log("Syncing");
+		return 0;
+	}
+}]);
