f876401a7b5fdf5854883a6b3ce867214989ae1b
truell20 <truell20@gmail.com>
2016-03-03 00:08:38 -0500

    Dont sync on entrance to database and sync tab

--- app/www/js/controllers.js
+++ app/www/js/controllers.js
@@ -1,23 +1,11 @@
 angular.module('starter.controllers', [])
 
 .controller('SyncCtrl', function($scope, $ionicHistory) {
-	$scope.$on('$ionicView.enter', function(e) {
-		sync()
-		$ionicHistory.goBack(-1);
-	});
+	
 })
 
 .controller('SubmitCtrl', function($scope) {})
 
 .controller('DataCtrl', function($scope, Chats) {
-	// With the new view caching in Ionic, Controllers are only called
-	// when they are recreated or on app start, instead of every page change.
-	// To listen for when this page is active (for example, to refresh data),
-	// listen for the $ionicView.enter event:
-	//
-	$scope.$on('$ionicView.enter', function(e) {
-		sync()
-		// fill in and stuff
-	});
 
 });
