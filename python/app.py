2412f839e0e4e79cfee9b986ea1179276bb4ba9e
Michael Truell <truell20@gmail.com>
2016-03-08 23:44:42 -0500

    Flask app beginnings

---
+++ python/app.py
@@ -0,0 +1,19 @@
+import os
+
+from flask import Flask, redirect, url_for, request, render_template
+from pymongo import MongoClient
+
+app = Flask(__name__)
+
+client = MongoClient()
+db = client.ScoutingApp
+
+@app.route('/api', methods=['GET', 'POST'])
+def team():
+	if request.method == 'GET':
+		return str([item for item in db.ScoutingApp.find()])
+	else:
+		db.ScoutingApp.insert_one(request.form)
+
+if __name__ == "__main__":
+	app.run(host='0.0.0.0', debug=True)
